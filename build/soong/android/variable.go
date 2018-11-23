3342141babcfdc0b7bee4f98e26c6b8ed3576808
Gokul Balram <gokulbalram@gmail.com>
2018-11-23 22:38:40 +0530

    exynos4 soong
    is this even right
    
    Change-Id: Ifb911774f1191fd552738e0bd13d779f11a8f83b

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,6 +1,16 @@
 package android
 type Product_variables struct {
+	target_board struct {
+		Cflags []string
+        }
+	use_samsung_color struct {
+		Cflags []string
+	}
 }
 
 type ProductVariables struct {
+	target_board *string `json:",omitempty"`
+	use_samsung_color  *bool `json:",omitempty"`
+
+
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -3,7 +3,8 @@ add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 _contents := $(_contents)    "Lineage":{$(newline)
 
  # See build/core/soong_config.mk for the add_json_* functions you can use here.
-$(call add_json_bool, Has_legacy_camera_hal1,                $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
+$(call add_json_str, target_board,                $(TARGET_BOARD_PLATFORM))
+$(call add_json_bool, use_samsung_color, $(filter true,$(BOARD_USE_SAMSUNG_COLORFORMAT)))
 
  # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END
